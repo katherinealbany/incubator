618018a60d0307b73d8857e2183008b9a11d3872
Katherine Albany <git@kalbany.net>
2017-02-13 09:10:13 +0000

    Checkpoint

--- ntp/run.sh
+++ ntp/run.sh
@@ -1,1 +1,5 @@
-rake -f ~/kalbany.net/drake/Rakefile && docker run -it --cap-add SYS_TIME --cap-add IPC_LOCK --ulimit memlock=-1:-1 katherinealbany/ntp
+rake -f ~/kalbany.net/drake/Rakefile
+
+# docker run -it --cap-add SYS_TIME --cap-add IPC_LOCK --ulimit memlock=-1:-1 katherinealbany/ntp
+
+docker stack deploy --compose-file docker-compose.yml ntp
