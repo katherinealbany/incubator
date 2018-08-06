cef4c9e1d2d08ff3a447643dcc45d85310e4127c
Katherine Albany <git@kalbany.net>
2018-08-06 17:02:07 +0100

    Checkpoint

--- helm/vault-rebuild.sh
+++ helm/vault-rebuild.sh
@@ -8,6 +8,6 @@ helm delete --purge vault
 
 ###################################################################################################
 
-helm install --debug --name vault --values vault-values.yaml ccoe/vault
+helm install --debug --name vault --values vault-values.yaml --set "${1}" ccoe/vault
 
 ###################################################################################################
