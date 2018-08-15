e445e4f86afcb59ac25b983a46bb2d95b64b84dc
Katherine Albany <git@kalbany.net>
2018-08-15 18:27:42 +0100

    Checkpoint

--- helm/cert-manager/install.sh
+++ helm/cert-manager/install.sh
@@ -1,5 +1,14 @@
 ###################################################################################################
 
-helm upgrade cert-manager stable/cert-manager --install --debug --wait --namespace cert-manager --version 0.4.1 --values "$(dirname ${BASH_SOURCE[0]})/values.yaml"
+BASE=$(dirname ${BASH_SOURCE[0]})
+
+###################################################################################################
+
+helm upgrade cert-manager stable/cert-manager --install --wait --namespace cert-manager --version 0.4.1 --values "${BASE}/values.yaml"
+
+###################################################################################################
+
+kubectl apply --record=true --wait=true --filename="${BASE}/lets-encrypt-staging.yaml"
+kubectl apply --record=true --wait=true --filename="${BASE}/lets-encrypt.yaml"
 
 ###################################################################################################
