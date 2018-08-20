e399a856682863f5b083744cdc9eb9dd661cd1e9
Katherine Albany <git@kalbany.net>
2018-08-20 15:50:55 +0100

    Checkpoint

--- helm/cert-manager/install.sh
+++ helm/cert-manager/install.sh
@@ -1,13 +1,9 @@
 ###################################################################################################
 
-BASE=$(dirname ${BASH_SOURCE[0]})
+helm upgrade cert-manager stable/cert-manager --install --wait --namespace cert-manager --version 0.4.1 --set "podDnsPolicy=None,podDnsConfig.nameservers={1.1.1.1,8.8.8.8}"
 
 ###################################################################################################
 
-helm upgrade cert-manager stable/cert-manager --install --wait --namespace cert-manager --version 0.4.1 --values "${BASE}/values.yaml"
-
-###################################################################################################
-
-kubectl apply --record=true --wait=true --filename="${BASE}/cluster-issuer-lets-encrypt.yaml"
+kubectl apply --record=true --wait=true --filename="$(dirname ${BASH_SOURCE[0]})/cluster-issuer-lets-encrypt.yaml"
 
 ###################################################################################################
