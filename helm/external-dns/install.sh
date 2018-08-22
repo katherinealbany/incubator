1f8168a994e7f73d6a7caa57586d6bea964cf6c1
Katherine Albany <git@kalbany.net>
2018-08-22 12:35:14 +0100

    Checkpoint

--- helm/external-dns/install.sh
+++ helm/external-dns/install.sh
@@ -1,9 +1,14 @@
 ###################################################################################################
 
+EXTERNAL_DNS_VERSION='0.7.1'
+
+###################################################################################################
+
 BASE="$(dirname ${BASH_SOURCE[0]})"
 
 ###################################################################################################
 
-helm upgrade external-dns stable/external-dns --install --debug --wait --namespace external-dns --version 0.7.1 --values "${BASE}/values.yaml"
+helm upgrade external-dns-public  stable/external-dns --install --debug --wait --namespace external-dns --version "${EXTERNAL_DNS_VERSION}" --values "${BASE}/public-values.yaml"
+helm upgrade external-dns-private stable/external-dns --install --debug --wait --namespace external-dns --version "${EXTERNAL_DNS_VERSION}" --values "${BASE}/private-values.yaml"
 
 ###################################################################################################
