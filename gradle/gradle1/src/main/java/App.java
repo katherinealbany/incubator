d7b7f1febac25f207bf87488d2d1f531d60dcfcc
Katherine Albany <git@kalbany.net>
2018-10-12 12:40:15 +0100

    Checkpoint

--- gradle/gradle1/src/main/java/App.java
+++ gradle/gradle1/src/main/java/App.java
@@ -1,13 +1,15 @@
 //-----------------------------------------------------------------------------------------------//
 
+package net.kalbany;
+
+//-----------------------------------------------------------------------------------------------//
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello world.");
     }
+
 }
 
 //-----------------------------------------------------------------------------------------------//
