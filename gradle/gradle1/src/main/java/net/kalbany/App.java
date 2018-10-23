63a46ea1f2fb5e5900b9d7da90c66af727d79612
Katherine Albany <git@kalbany.net>
2018-10-23 11:44:31 +0100

    Checkpoint

--- gradle/gradle1/src/main/java/net/kalbany/App.java
+++ gradle/gradle1/src/main/java/net/kalbany/App.java
@@ -1,13 +1,14 @@
 package net.kalbany;
 
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App {
 
-   private static final Logger LOG = Logger.getLogger(App.class.getName());
+   private static final Logger LOGGER = LogManager.getLogger(App.class);
 
    public static void main (String[] args) {
-      LOG.info("Hello world.");
+      LOGGER.info("Hello world.");
    }
 
 }
