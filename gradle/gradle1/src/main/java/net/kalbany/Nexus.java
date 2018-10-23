3c3674210005ccf5a70608cfc095d905d600887f
Katherine Albany <git@kalbany.net>
2018-10-23 15:00:18 +0100

    Checkpoint

--- gradle/gradle1/src/main/java/net/kalbany/Nexus.java
+++ gradle/gradle1/src/main/java/net/kalbany/Nexus.java
@@ -1,11 +1,11 @@
 package net.kalbany;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-public class App {
+public class Nexus {
 
-   private static final Logger LOGGER = LogManager.getLogger(App.class);
+   private static final Logger LOGGER = LogManager.getLogger(Nexus.class);
 
    public static void main (String[] args) {
       LOGGER.info("Hello world.");
