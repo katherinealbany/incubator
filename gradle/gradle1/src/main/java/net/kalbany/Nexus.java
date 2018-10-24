8a36145e162573d6db6949e036bb5b2508e47d5b
Katherine Albany <git@kalbany.net>
2018-10-24 15:51:57 +0100

    Checkpoint

--- gradle/gradle1/src/main/java/net/kalbany/Nexus.java
+++ gradle/gradle1/src/main/java/net/kalbany/Nexus.java
@@ -1,14 +1,38 @@
 package net.kalbany;
 
+import java.util.List;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+
+import com.amazonaws.services.sqs.model.Message;
+
 public class Nexus {
 
-   private static final Logger LOGGER = LogManager.getLogger(Nexus.class);
+   private static final Logger logger = LogManager.getLogger(Nexus.class);
 
    public static void main (String[] args) {
-      LOGGER.info("Hello world.");
+      final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+      try {
+         final String url = sqs.getQueueUrl("s3-events-travisperkins-logging-ccoe-cloudtrail").getQueueUrl();
+
+         do {
+            List<Message> messages = sqs.receiveMessage(url).getMessages();
+
+            for (Message m : messages) {
+               logger.info(m.getBody());
+               sqs.deleteMessage(url, m.getReceiptHandle());
+            }
+         }
+         while (true);
+      }
+      finally {
+         sqs.shutdown();
+      }
    }
 
 }
