9a4c10cb7f4165c240edcdf63f727038f05a2888
Katherine Albany <git@kalbany.net>
2018-10-25 21:07:32 +0100

    Checkpoint

--- go/go1/main.go
+++ go/go1/main.go
@@ -1,9 +1,68 @@
 package main
 
 import (
+	"fmt"
+
 	log "github.com/sirupsen/logrus"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 func main() {
-	log.Info("hello world")
+	sess, err := session.NewSession(&aws.Config{})
+
+	svc := sqs.New(sess)
+
+	result0, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: aws.String("s3-events-travisperkins-logging-ccoe-cloudtrail"),
+	})
+
+	if err != nil {
+		log.Fatal("Error", err)
+		return
+	}
+
+	log.Info(*result0.QueueUrl)
+
+	// URL to our queue
+	qURL := *result0.QueueUrl
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &qURL,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(20), // 20 seconds
+		WaitTimeSeconds:     aws.Int64(0),
+	})
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	if len(result.Messages) == 0 {
+		fmt.Println("Received no messages")
+		return
+	}
+
+	log.Info(result.Messages[0])
+
+	resultDelete, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      &qURL,
+		ReceiptHandle: result.Messages[0].ReceiptHandle,
+	})
+
+	if err != nil {
+		fmt.Println("Delete Error", err)
+		return
+	}
+
+	fmt.Println("Message Deleted", resultDelete)
 }
