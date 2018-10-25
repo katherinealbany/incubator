594812de6a15774a5163e08cd285f09e671561fe
Katherine Albany <git@kalbany.net>
2018-10-25 16:16:57 +0100

    Checkpoint

--- gradle/gradle1/src/main/java/net/kalbany/Nexus.java
+++ gradle/gradle1/src/main/java/net/kalbany/Nexus.java
@@ -1,6 +1,9 @@
 package net.kalbany;
 
 import java.util.List;
+import java.util.Iterator;
+
+import java.io.IOException;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -9,6 +12,9 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 
 import com.amazonaws.services.sqs.model.Message;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Nexus {
 
@@ -19,16 +25,44 @@ public class Nexus {
 
       try {
          final String url = sqs.getQueueUrl("s3-events-travisperkins-logging-ccoe-cloudtrail").getQueueUrl();
+         final ObjectMapper objectMapper = new ObjectMapper();
 
          do {
             List<Message> messages = sqs.receiveMessage(url).getMessages();
 
             for (Message m : messages) {
-               logger.info(m.getBody());
+               logger.debug("----- rootNode");
+               JsonNode rootNode = objectMapper.readTree(m.getBody());
+               logger.debug(rootNode.toString());
+
+               logger.debug("----- recordsNode");
+               JsonNode recordsNode = rootNode.path("Records");
+               logger.debug(recordsNode.toString());
+
+               Iterator<JsonNode> elements = recordsNode.elements();
+               while (elements.hasNext()) {
+                  JsonNode element = elements.next();
+
+                  logger.debug("----- s3Node");
+                  JsonNode s3Node = element.path("s3");
+                  logger.debug(s3Node.toString());
+
+                  logger.debug("----- object");
+                  JsonNode objectNode = s3Node.path("object");
+                  logger.debug(objectNode.toString());
+
+                  logger.debug("----- key");
+                  JsonNode keyNode = objectNode.path("key");
+                  logger.info(keyNode.asText());
+               }
+
                sqs.deleteMessage(url, m.getReceiptHandle());
             }
          }
          while (true);
+      }
+      catch (IOException ex) {
+         logger.error(ex.getMessage(), ex);
       }
       finally {
          sqs.shutdown();
