cbe9d4ac5af232f3c965b59e70e99757651d0ed7
Katherine Albany <git@kalbany.net>
2018-11-20 15:24:56 +0000

    Checkpoint

--- work/sandpit/external-dns/install.sh
+++ work/sandpit/external-dns/install.sh
@@ -7,13 +7,12 @@ BASE="$(dirname ${BASH_SOURCE[0]})"
 
 ###################################################################################################
 
-[[ -z "${EXTERNAL_DNS_VERSION}"                  ]] && EXTERNAL_DNS_VERSION='1.0.2'
-[[ -z "${EXTERNAL_DNS_TXT_OWNER_ID_SALT}"        ]] && EXTERNAL_DNS_TXT_OWNER_ID_SALT='default'
-[[ -z "${EXTERNAL_DNS_PUBLIC_ANNOTATION_FILTER}" ]] && EXTERNAL_DNS_PUBLIC_ANNOTATION_FILTER='external-dns/public=true'
+[[ -z "${EXTERNAL_DNS_VERSION}"           ]] && EXTERNAL_DNS_VERSION='1.0.2'
+[[ -z "${EXTERNAL_DNS_TXT_OWNER_ID_SALT}" ]] && EXTERNAL_DNS_TXT_OWNER_ID_SALT='default'
 
 ###################################################################################################
 
-helm upgrade external-dns-public  stable/external-dns --install --debug --wait --namespace external-dns --version "${EXTERNAL_DNS_VERSION}" --values "${BASE}/public-values.yaml"  --set txtOwnerId=external-dns-public-${EXTERNAL_DNS_TXT_OWNER_ID_SALT} --set "annotationFilter=${EXTERNAL_DNS_PUBLIC_ANNOTATION_FILTER}"
+helm upgrade external-dns-public  stable/external-dns --install --debug --wait --namespace external-dns --version "${EXTERNAL_DNS_VERSION}" --values "${BASE}/public-values.yaml"  --set txtOwnerId=external-dns-public-${EXTERNAL_DNS_TXT_OWNER_ID_SALT}
 helm upgrade external-dns-private stable/external-dns --install --debug --wait --namespace external-dns --version "${EXTERNAL_DNS_VERSION}" --values "${BASE}/private-values.yaml" --set txtOwnerId=external-dns-private-${EXTERNAL_DNS_TXT_OWNER_ID_SALT}
 
 ###################################################################################################
