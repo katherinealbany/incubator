943b93b2622c7762c316fe89163459ee56194a15
Katherine Albany <git@kalbany.net>
2018-11-21 16:52:18 +0000

    Checkpoint

--- work/sandpit/monitoring/install.sh
+++ work/sandpit/monitoring/install.sh
@@ -18,3 +18,7 @@ BASE="$(dirname ${BASH_SOURCE[0]})"
 helm upgrade monitoring stable/prometheus-operator --install --debug --namespace monitoring --version "${PROMETHEUS_OPERATOR_VERSION}" --values "${BASE}/values.yaml" --set "prometheusOperator.createCustomResource=${CREATE_CUSTOM_RESOURCE}"
 
 ###################################################################################################
+
+# https://github.com/helm/charts/tree/master/stable/prometheus-cloudwatch-exporter
+
+###################################################################################################
