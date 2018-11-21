3fda23d066e4f375ae209eaad5fc020a935fea65
Katherine Albany <git@kalbany.net>
2018-11-21 16:49:33 +0000

    Checkpoint

--- work/sandpit/monitoring/install.sh
+++ work/sandpit/monitoring/install.sh
@@ -7,10 +7,14 @@ BASE="$(dirname ${BASH_SOURCE[0]})"
 
 ###################################################################################################
 
-[[ -z "${PROMETHEUS_OPERATOR_VERSION}" ]] && PROMETHEUS_OPERATOR_VERSION='0.1.23'
+[[ -z "${PROMETHEUS_OPERATOR_VERSION}" ]] && PROMETHEUS_OPERATOR_VERSION='0.1.25'
 
 ###################################################################################################
 
-helm upgrade monitoring stable/prometheus-operator --install --debug --namespace monitoring --version "${PROMETHEUS_OPERATOR_VERSION}" --values "${BASE}/values.yaml"
+[[ $(kubectl get crd --output=custom-columns=:.metadata.name | grep monitoring.coreos.com | wc -l | awk '{print $1}') -eq 0 ]] && CREATE_CUSTOM_RESOURCE=true || CREATE_CUSTOM_RESOURCE=false
+
+###################################################################################################
+
+helm upgrade monitoring stable/prometheus-operator --install --debug --namespace monitoring --version "${PROMETHEUS_OPERATOR_VERSION}" --values "${BASE}/values.yaml" --set "prometheusOperator.createCustomResource=${CREATE_CUSTOM_RESOURCE}"
 
 ###################################################################################################
