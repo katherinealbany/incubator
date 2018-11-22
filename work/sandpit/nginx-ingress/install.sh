0398fd4d4dd62888345cbe8286acad773617ca4c
Katherine Albany <git@kalbany.net>
2018-11-22 13:56:00 +0000

    Checkpoint

--- work/sandpit/nginx-ingress/install.sh
+++ work/sandpit/nginx-ingress/install.sh
@@ -14,3 +14,9 @@ BASE="$(dirname ${BASH_SOURCE[0]})"
 helm upgrade nginx-ingress stable/nginx-ingress --install --debug --wait --namespace nginx-ingress --version "${NGINX_INGRESS_VERSION}" --values "${BASE}/values.yaml"
 
 ###################################################################################################
+
+if [[ $(kubectl get customresourcedefinitions --output=custom-columns=:.metadata.name | grep servicemonitors.monitoring.coreos.com | wc -l | awk '{print $1}') -eq 1 ]]; then
+  kubectl apply --record=true --wait=true --filename="${BASE}/service-monitor.yaml"
+fi
+
+###################################################################################################
