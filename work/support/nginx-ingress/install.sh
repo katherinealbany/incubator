ef97b2bf9ea7d28271e29cc29303301144b9ca7f
Katherine Albany <git@kalbany.net>
2018-11-23 16:31:06 +0000

    Checkpoint

---
+++ work/support/nginx-ingress/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+set -o pipefail
+
+###################################################################################################
+
+BASE="$(dirname ${BASH_SOURCE[0]})"
+
+###################################################################################################
+
+[[ -z "${NGINX_INGRESS_VERSION}" ]] && NGINX_INGRESS_VERSION='0.31.0'
+
+###################################################################################################
+
+helm upgrade nginx-ingress stable/nginx-ingress --install --debug --wait --namespace nginx-ingress --version "${NGINX_INGRESS_VERSION}" --values "${BASE}/values.yaml"
+
+###################################################################################################
+
+if [[ $(kubectl get customresourcedefinitions --output=custom-columns=:.metadata.name | grep servicemonitors.monitoring.coreos.com | wc -l | awk '{print $1}') -eq 1 ]]; then
+  kubectl apply --record=true --wait=true --filename="${BASE}/service-monitor.yaml"
+fi
+
+###################################################################################################
