b194084bffce43457e527ea737b1d028a967275e
Katherine Albany <git@kalbany.net>
2019-01-02 10:48:38 +0000

    Checkpoint

--- work/sandpit/helm/install.sh
+++ work/sandpit/helm/install.sh
@@ -1,1 +1,17 @@
-../../support/helm/install.sh
+#!/bin/bash -ex
+set -o pipefail
+
+###################################################################################################
+
+BASE="$(dirname ${BASH_SOURCE[0]})"
+
+###################################################################################################
+
+kubectl apply --record=true --wait=true --filename="${BASE}/service-account.yaml"
+kubectl apply --record=true --wait=true --filename="${BASE}/cluster-role-binding.yaml"
+
+###################################################################################################
+
+helm init --upgrade --debug --wait --tiller-namespace kube-system --service-account tiller --history-max 10 --replicas 3
+
+###################################################################################################
