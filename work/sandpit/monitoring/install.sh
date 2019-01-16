5fe20ff616a32e6f9bab3aef823d2ed7ea94b375
Katherine Albany <git@kalbany.net>
2019-01-16 14:48:43 +0000

    Checkpoint

--- work/sandpit/monitoring/install.sh
+++ work/sandpit/monitoring/install.sh
@@ -1,1 +1,24 @@
-../../support/monitoring/install.sh
+#!/bin/bash -ex
+set -o pipefail
+
+###################################################################################################
+
+BASE="$(dirname ${BASH_SOURCE[0]})"
+
+###################################################################################################
+
+[[ -z "${PROMETHEUS_OPERATOR_VERSION}" ]] && PROMETHEUS_OPERATOR_VERSION='0.1.29'
+
+###################################################################################################
+
+[[ $(kubectl get customresourcedefinitions --output=custom-columns=:.metadata.name | grep monitoring.coreos.com | wc -l | awk '{print $1}') -eq 0 ]] && CREATE_CUSTOM_RESOURCE=true || CREATE_CUSTOM_RESOURCE=false
+
+###################################################################################################
+
+helm upgrade monitoring stable/prometheus-operator --install --debug --namespace monitoring --version "${PROMETHEUS_OPERATOR_VERSION}" --values "${BASE}/values.yaml" --set "prometheusOperator.createCustomResource=${CREATE_CUSTOM_RESOURCE}"
+
+###################################################################################################
+
+kubectl apply --record=true --wait=true --filename="${BASE}/service.yaml"
+
+###################################################################################################
