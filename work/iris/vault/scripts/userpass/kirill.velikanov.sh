7531c02cc35373af8ce73b905a8c3aeb8fb08462
Katherine Albany <git@kalbany.net>
2019-04-03 18:28:23 +0100

    Checkpoint

---
+++ work/iris/vault/scripts/userpass/kirill.velikanov.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+set -o pipefail
+
+###################################################################################################
+
+USERNAME='kirill.velikanov'
+EMAIL_ADDRESS='kyrylo.velykanov@travisperkins.co.uk'
+
+###################################################################################################
+
+vault write identity/entity name=${USERNAME} metadata=email=${EMAIL_ADDRESS}
+
+###################################################################################################
+
+if [ $(vault read /auth/userpass/users/${USERNAME} > /dev/null 2>&1 ; echo ${?}) -ne 0 ]; then
+  vault write identity/entity-alias name=${USERNAME} canonical_id=$(vault read -format=json /identity/entity/name/${USERNAME} | jq -r '.data.id') mount_accessor=$(vault auth list -format=json | jq -r '.[] | select(.type == "userpass") | .accessor')
+  PASSWORD=$(vault write -format=json /sys/tools/random bytes=5 format=hex | jq -r '.data.random_bytes')
+  vault write auth/userpass/users/${USERNAME} password=${PASSWORD}
+  echo "Initial Random Password [${PASSWORD}]"
+fi
+
+###################################################################################################
