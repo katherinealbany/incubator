67a2dd6d9b90e33b76e4752964ef3a7b6b06a3d9
Katherine Albany <git@kalbany.net>
2019-04-04 11:32:11 +0100

    Checkpoint

--- work/iris/vault/scripts/identity/group/digital-micro-services-writers.sh
+++ work/iris/vault/scripts/identity/group/digital-micro-services-writers.sh
@@ -3,6 +3,25 @@ set -o pipefail
 
 ###################################################################################################
 
-vault write identity/group name=digital-micro-services-writers policies=administrator member_entity_ids=$(vault read -format=json /identity/entity/name/katherine-albany | jq -r '.data.id')
+POLICIES='kv-digital-reader,kv-digital-writer,kv-digital-micro-services-reader,kv-digital-micro-services-writer'
+MEMBER_ENTITY_NAMES='kirill.velikanov,sergey.evstifeev'
+
+###################################################################################################
+
+FIRST_ITERATION='true'
+unset MEMBER_ENTITY_IDS
+for ENTITY_NAME in $(echo "${MEMBER_ENTITY_NAMES}" | sed 's/,/ /g'); do
+  ENTITY_ID=$(vault read -format=json identity/entity/name/${ENTITY_NAME} | jq -r '.data.id')
+  if [ "${FIRST_ITERATION}" == 'true' ]; then
+    MEMBER_ENTITY_IDS="${ENTITY_ID}"
+    FIRST_ITERATION='false'
+  else
+    MEMBER_ENTITY_IDS="${MEMBER_ENTITY_IDS},${ENTITY_ID}"
+  fi
+done
+
+###################################################################################################
+
+vault write identity/group/name/digital-micro-services-writers policies="${POLICIES}" member_entity_ids="${MEMBER_ENTITY_IDS}"
 
 ###################################################################################################
